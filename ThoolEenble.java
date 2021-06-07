4b5a118089114ad07445e0fdad0ecc7d803a0ca0
tansuo <1479183941@qq.com>
2021-06-07 13:40:20 +0800

    ThoolEenble.java

---
+++ ThoolEenble.java
@@ -0,0 +1,16 @@
+public  class ThoolEenble  {
+    public static void main(String[] args) {
+        IntBuffer intBuffer = IntBuffer.allocate(30);
+        for (int i = 0; i < intBuffer.capacity() ; ++i) {
+            int j = 2 *(i + 1);
+            // 缓冲区重要属性： position limit capacity
+            intBuffer.put(j); // put:数据写入缓冲区
+        }
+        intBuffer.flip();
+        while (intBuffer.hasRemaining()){
+            int j = intBuffer.get(); // get:数据写入缓冲区
+            System.out.print(j + "   ");
+        }
+    }
+
+}
